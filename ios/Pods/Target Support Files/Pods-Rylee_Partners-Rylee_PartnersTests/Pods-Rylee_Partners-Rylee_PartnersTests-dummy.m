03d29a4be994f0334d0486d81012f3171d264375
vimal <vimalnavadiya2705@gmail.com>
2023-04-11 11:27:54 +0530

    commit 3
    - android-and-ios

---
+++ ios/Pods/Target Support Files/Pods-Rylee_Partners-Rylee_PartnersTests/Pods-Rylee_Partners-Rylee_PartnersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rylee_Partners_Rylee_PartnersTests : NSObject
+@end
+@implementation PodsDummy_Pods_Rylee_Partners_Rylee_PartnersTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Rylee_Partners/Pods-Rylee_Partners-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rylee_Partners : NSObject
+@end
+@implementation PodsDummy_Pods_Rylee_Partners
+@end
